27cb39cce2c4e52c4480b62f373fa5f980c58900
Dharmi26 <dharmipatel567@gmail.com>
2023-06-14 23:50:36 +0530

    h

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import '../styles/project.css'
 export default function Project(){
     return(
         <>
-            <h1>Projects</h1>
+            <div className='bk position-relative w-100'>
+
+                <img src={require("../assets/Home.png")} className='w-100'/>
+
+                <div className='position-absolute top-0 start-50 mt-5 mb-5 d-flex flex-row gap-4 flex-wrap justify-content-center align-items-center' style={{backgroundColor:"transparent"}}>
+                    <div className='w-50 border-none rounded-2 shadow p-3'>p1</div>
+                    <div className='w-50 border-none rounded-2 shadow p-3'>p2</div>
+                    <div className='w-50 border-none rounded-2 shadow p-3'>p3</div>
+                    <div className='w-50 border-none rounded-2 shadow p-3'>p4</div>
+                </div>
+            </div>
         </>
     )
 }
