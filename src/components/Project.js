ccf609553bd481253e4e048a3bd35ea08c93dd5e
Dharmi26 <dharmipatel567@gmail.com>
2023-06-17 00:14:24 +0530

    project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import '../styles/project.css'
+import '../styles/project.css';
+import { Link } from 'react-router-dom';
 export default function Project(){
     return(
         <>
-            <div className='bk position-relative w-100'>
-
-                <img src={require("../assets/Home.png")} className='w-100'/>
-
-                <div className='position-absolute top-0 start-50 mt-5 mb-5 d-flex flex-row gap-4 flex-wrap justify-content-center align-items-center' style={{backgroundColor:"transparent"}}>
-                    <div className='w-50 border-none rounded-2 shadow p-3'>p1</div>
-                    <div className='w-50 border-none rounded-2 shadow p-3'>p2</div>
-                    <div className='w-50 border-none rounded-2 shadow p-3'>p3</div>
-                    <div className='w-50 border-none rounded-2 shadow p-3'>p4</div>
+            <div className="mt-5 mb-5">
+                <p className="fw-semibold" style={{color:"#3C5186",fontStyle:"italic"}}>No idea is too small, and no problem is too big,"</p>
+            </div>
+            <div className="position-relative container">
+                <div className='d-none d-md-block'>
+                    <img src={require("../assets/Home.png")} className='w-100'/>
+                </div>
+                <div className="position-absolute start-0 top-0 mt-4 row" style={{backgroundColor:"transparent"}}>
+                    <div className="col-md-6 mb-5" style={{backgroundColor:"transparent"}}>
+                        <div className="image-container" style={{backgroundColor:"transparent"}}>
+                        <img src={require("../assets/P1.jpeg")} alt="Image 3" className="img-fluid w-75"/>
+                        <div className="overlay">
+                            <Link to="https://drive.google.com/file/d/1EN15y4NxcDG8Ni_kAQL9SxopR5_UFP85/view?usp=sharing" target="_blank"><button className='btnn1'>View Project</button></Link>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 mb-5" style={{backgroundColor:"transparent"}}>
+                        <div className="image-container" style={{backgroundColor:"transparent"}}>
+                        <img src={require("../assets/P2.jpeg")} alt="Image 3" className="img-fluid w-75"/>
+                        <div className="overlay">
+                            <Link to="https://frontend-nine-ochre.vercel.app/" target="_blank"><button className='btnn1'>View Project</button></Link>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 mb-5" style={{backgroundColor:"transparent"}}>
+                        <div className="image-container" style={{backgroundColor:"transparent"}}>
+                        <img src={require("../assets/P3.jpeg")} alt="Image 3" className="img-fluid w-75"/>
+                        <div className="overlay">
+                            <Link to="https://remine-india-git-master-dharmi26.vercel.app/" target="_blank"><button className="btnn1">View Project</button></Link>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 mb-5" style={{backgroundColor:"transparent"}}>
+                        <div className="image-container" style={{backgroundColor:"transparent"}}>
+                        <img src={require("../assets/P4.jpeg")} alt="Image 3" className="img-fluid w-75"/>
+                        <div className="overlay">
+                            <Link to="https://zomato-landing-page-seven.vercel.app/" target="_blank"><button className="btnn1">View Project</button></Link>
+                        </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
